baa031a3bed75ce2b082b2288725eb477f2f0fc3
Oleg <pirr31@mail.ru>
2021-03-01 23:42:57 +0300

    fix: change dateSample function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,5 +4,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  return Math.ceil( (Math.log(MODERN_ACTIVITY / sampleActivity)) / (0.693 / HALF_LIFE_PERIOD) );
+  if (typeof(sampleActivity) != 'string' || isNaN(sampleActivity) || Number(sampleActivity) <= 0 || Number(sampleActivity) > MODERN_ACTIVITY) return 0;
+  
+  return Math.ceil( ( Math.log( MODERN_ACTIVITY / Number(sampleActivity) ) ) / ( 0.693 / HALF_LIFE_PERIOD ) );
 };
